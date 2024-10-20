c83fb0e238a97f32d2557e74eec4ffe510887ad0
Qhojoblinks-7 <immanueleshun9@gmail.com>
2024-10-20 11:45:13 +0000

    added the createAddQuoteForm

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,35 +4,54 @@ const quotes = [
     { text: "Life is what happens when you're busy making other plans.", category: "life" }
   ];
 
+  //show random quotes
   function showRandomQuote(){
-    const randomIndex = Math.floor(Math.random()*quotes.length); //get random index
+    //get random index
+    const randomIndex = Math.floor(Math.random()*quotes.length); 
     const randomQuote = quotes[randomIndex];
 
     const displayQuotes = document.getElementById('quoteDisplay');
     displayQuotes.innerHTML = `<p>${randomQuote.text}</p><p><em>Category: ${randomQuote.category}</em></p>`;
   }
 
+  //add event listener to show random quotes
   const newQuoteButton = document.getElementById('newQuote').addEventListener('click', showRandomQuote);
 
-  
-
-  function addQuote(){
+  //creating a function to add quotes in forms
+  function createAddQuoteForm(){
     const inputQuote = document.getElementById('newQuoteText').value.trim();
-  const inputCategory = document.getElementById('newQuoteCategory').value.trim();
+    const inputCategory = document.getElementById('newQuoteCategory').value.trim();
 
     const newQuote = inputQuote;
     const category = inputCategory;
 
     if (newQuote && category){
 
+      //add new quotes to the array
         quotes.push({text:newQuote, category: category});
+
+        //adding new quotes to the local storage
         localStorage.setItem('quotes', JSON.stringify(quotes));
+
+        //changing input into json data
         JSON.parse(localStorage.getItem('quotes'))
+
+        //notify user
         alert("New quote added");
+
+        //resetting the form for new input
         reset();
+
+        //calling showRadomQuote
+        showRandomQuote();
     }else{
         alert("Please fill in both the quote and category fields.");
     }
+  }
+
+  function addQuote(event){
+    event.preventDefault();
+    createAddQuoteForm();
   }
 
   // Function to reset the input fields

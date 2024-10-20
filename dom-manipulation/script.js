6e1280ca64c80c6481d4d82d2c75fcfad275b360
Qhojoblinks-7 <immanueleshun9@gmail.com>
2024-10-20 12:19:39 +0000

    added a new function to upload and process uploaded files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -74,4 +74,15 @@ function reset() {
     document.getElementById('newQuoteCategory').value = '';
 }
 
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert('Quotes imported successfully!');
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
+
 
